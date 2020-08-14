9bc94cb44e3e2588ebded104b40fccde61ef21b6
Nathan <nathanpykimutai@gmail.com>
2020-08-14 16:57:44 +0300

    accessing and looping throught the variables elements

--- chapter1/variables.dart
+++ chapter1/variables.dart
@@ -4,10 +4,30 @@ void main() {
   var name = "Nathan";
   var age = 20;
   var schools = ["Mokwo","Kapsabet","Moringa","KCA"];
-  var schools_level = {
+  var levels = {
     "Mokwo": "Primary",
     "Kapsabet": "HighSchool",
     "Moringa": "BootCamp",
     "KCA": "University"
   };
+
+  print(name);
+  print(age);
+  print(schools);
+  print(levels);
+
+  //accessing a single element in the arr
+  var el = schools[0];
+  print(el);
+
+  //accessing a single element in the arr
+  print(levels['Mokwo']);
+
+  //loop through the arr
+  for(var school in schools) {
+    print(school);
+  }
+
+  //loop through map
+  levels.forEach((k,v) => print("${k}:${v}"));
 }
