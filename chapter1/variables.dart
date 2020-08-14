75bf41da0750b0438e7f0be9390552f441882716
Nathan <nathanpykimutai@gmail.com>
2020-08-14 16:36:28 +0300

    in this commit we learned about creating of variable in dart

---
+++ chapter1/variables.dart
@@ -0,0 +1,13 @@
+//We are looking at variables in dart
+//Variables can hold any time of data be it strings,integers or arrays
+void main() {
+  var name = "Nathan";
+  var age = 20;
+  var schools = ["Mokwo","Kapsabet","Moringa","KCA"];
+  var schools_level = {
+    "Mokwo": "Primary",
+    "Kapsabet": "HighSchool",
+    "Moringa": "BootCamp",
+    "KCA": "University"
+  };
+}
